0708e2d94b3d0335de6cf1dee4291ace67a1b24f
Tananjay <83783632+Tananjay-git@users.noreply.github.com>
2025-06-26 10:13:45 +0530

    Update basic_question.sql

--- basic_question.sql
+++ basic_question.sql
@@ -43,3 +43,80 @@ SELECT department, avg(salary) FROM employees GROUP by department;
 
 --Find the highest salary in the entire table.
 SELECT * from employees ORDER by salary desc limit 1;
+
+--Get the number of employees in each department.
+SELECT department, count(name) as NUmber_of_employees 
+from employees
+GROUP by department;
+
+--Show total salary expense per department.
+SELECT department, salary FROM employees GROUP by department;
+
+--Find departments with more than 2 employees.
+SELECT department, count(emp_id) as number_of_emp FROM employees
+GROUP by department
+HAVING number_of_emp > 2;
+
+--Get departments where total salary exceeds 150,000.
+SELECT department, sum(salary) as total_salary 
+from employees
+GROUP by department
+having total_salary > 150000;
+
+--Find departments with average salary above 70,000.
+SELECT department, AVG(salary) as average_salary
+FROM employees
+GROUP by department
+having  average_salary > 70000;
+
+--Find how many projects each employee is working on.
+SELECT p.project_name, count(e.emp_id)
+from employees e
+join projects p 
+on e.emp_id = p.emp_id
+GROUP by 1;
+
+--List project count per department.
+SELECT e.department, count(p.project_id) as project_count
+from employees e 
+join projects p 
+on e.emp_id = p.emp_id
+GROUP by 1;
+
+--Find total salary of employees working on at least 1 project.
+SELECT e.name, sum(e.salary) as total_salary
+from employees e 
+join projects p 
+on e.emp_id = p.emp_id
+GROUP by e.name
+HAVING count(p.project_id) >= 1;
+
+--Assign a row number to each employee ordered by salary (highest first).
+SELECT * , row_number() over(partition by emp_id ORDER by salary DESC) as row_number
+from employees
+ORDER by row_number desc;
+
+--Rank employees within each department by salary.
+SELECT *, rank() over(PARTITION BY department ORDER by salary DESC)as rank
+from employees
+
+--Find the top 2 highest-paid employees in each department.
+select name, department, salary 
+from employees
+QUALIFY rank() over(partition by department ORDER by salary desc) <= 2 
+
+--For each employee, show how their salary compares to the department average.
+SELECT 
+	name
+    , salary
+    , avg(salary) over(partition by department) as dept_avg_salary
+    , salary - avg(salary) over(partition by department) as diff_from_avg
+from employees
+
+--Show each employee’s salary and the department's highest salary.
+SELECT 
+	name
+    , department
+    , salary
+    , max(salary) over(partition by department ) as department_highest_salary
+from employees
